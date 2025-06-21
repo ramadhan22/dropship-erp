8215fe1de35ee537c19fdf136d79303fd81c26da
Muhammad Ramadhan <muhammad.ramadhan40@gmail.com>
2025-06-21 22:11:25 +0700

    fix uuid insert types in 2025 expense data migration

--- backend/internal/migrations/0034_insert_expense_data_2025.up.sql
+++ backend/internal/migrations/0034_insert_expense_data_2025.up.sql
@@ -10,23 +10,23 @@ DECLARE
     cogs INT := (SELECT account_id FROM accounts WHERE account_code='5.1');
     loan INT := (SELECT account_id FROM accounts WHERE account_code='5.2.1');
     capex INT := (SELECT account_id FROM accounts WHERE account_code='5.2.8');
-    ids TEXT[] := ARRAY[
-      '389a5e77-985c-4cb1-8be2-b16c254080b3',
-      '7e06266c-a895-4b42-9049-defc62176e6d',
-      '245eccdd-dc5c-4083-ab37-3087fc6c3851',
-      '447dcdd7-bf3f-46cd-a709-f649cbd6efa0',
-      '7e817fbb-c39e-4be6-b5ac-79774db864bc',
-      'afbf2275-555d-4673-b99d-6a59680dde17',
-      '2ac23cd1-e62c-4bdb-935e-4365a702deff',
-      '0187d429-3f4b-4b5a-9675-f580ad1462a7',
-      '0fc33c2c-8e08-4fb9-baf5-fdb97460ce82',
-      '91ef8400-84e8-433b-98dc-0e89cffcebf3',
-      'c5fdd8e7-d6b0-48c9-a846-5bc63ae3059f',
-      '9d330096-7e4c-4160-a8b1-1f84b3faa23c',
-      '8b83b93c-e92f-416b-990e-e0cda3c6e906',
-      '56607835-25c9-49cf-9f64-791ae0735187',
-      '9bafd483-c30a-42a7-a679-2cf6af172eb5',
-      '2d9cb21e-3cd6-45d2-8f38-31fd65dfd068'
+    ids UUID[] := ARRAY[
+      '389a5e77-985c-4cb1-8be2-b16c254080b3'::uuid,
+      '7e06266c-a895-4b42-9049-defc62176e6d'::uuid,
+      '245eccdd-dc5c-4083-ab37-3087fc6c3851'::uuid,
+      '447dcdd7-bf3f-46cd-a709-f649cbd6efa0'::uuid,
+      '7e817fbb-c39e-4be6-b5ac-79774db864bc'::uuid,
+      'afbf2275-555d-4673-b99d-6a59680dde17'::uuid,
+      '2ac23cd1-e62c-4bdb-935e-4365a702deff'::uuid,
+      '0187d429-3f4b-4b5a-9675-f580ad1462a7'::uuid,
+      '0fc33c2c-8e08-4fb9-baf5-fdb97460ce82'::uuid,
+      '91ef8400-84e8-433b-98dc-0e89cffcebf3'::uuid,
+      'c5fdd8e7-d6b0-48c9-a846-5bc63ae3059f'::uuid,
+      '9d330096-7e4c-4160-a8b1-1f84b3faa23c'::uuid,
+      '8b83b93c-e92f-416b-990e-e0cda3c6e906'::uuid,
+      '56607835-25c9-49cf-9f64-791ae0735187'::uuid,
+      '9bafd483-c30a-42a7-a679-2cf6af172eb5'::uuid,
+      '2d9cb21e-3cd6-45d2-8f38-31fd65dfd068'::uuid
     ];
     dates DATE[] := ARRAY[
       '2025-01-19','2025-01-20','2025-01-31','2025-01-31','2025-01-31',
