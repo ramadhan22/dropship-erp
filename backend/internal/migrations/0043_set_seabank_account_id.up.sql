b2aa01b988b749368d38b356c873760e4038219d
Muhammad Ramadhan <muhammad.ramadhan40@gmail.com>
2025-07-02 01:23:06 +0700

    fix

--- backend/internal/migrations/0043_set_seabank_account_id.up.sql
+++ backend/internal/migrations/0043_set_seabank_account_id.up.sql
@@ -8,7 +8,7 @@ BEGIN
     END IF;
     IF old_id <> 11014 THEN
         UPDATE journal_lines SET account_id=11014 WHERE account_id=old_id;
-        UPDATE expenses SET account_id=11014 WHERE account_id=old_id;
+        UPDATE expenses SET asset_account_id=11014 WHERE asset_account_id=old_id;
         UPDATE asset_accounts SET account_id=11014 WHERE account_id=old_id;
         UPDATE accounts SET account_id=11014 WHERE account_code='1.1.14';
         PERFORM setval('accounts_account_id_seq', (SELECT MAX(account_id) FROM accounts));
