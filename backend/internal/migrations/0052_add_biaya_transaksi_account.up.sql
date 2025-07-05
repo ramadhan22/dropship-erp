a22a3c69c689c4774d1fa35b2ec8a1340108c5f0
Muhammad Ramadhan <muhammad.ramadhan40@gmail.com>
2025-07-05 21:42:02 +0700

    fix

--- backend/internal/migrations/0052_add_biaya_transaksi_account.up.sql
+++ backend/internal/migrations/0052_add_biaya_transaksi_account.up.sql
@@ -2,7 +2,7 @@ DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM accounts WHERE account_code='5.2.11') THEN
         INSERT INTO accounts (account_id, account_code, account_name, account_type, parent_id)
-        VALUES (52011, '5.2.11', 'Biaya Transaksi', 'Expense',
+        VALUES (52012, '5.2.11', 'Biaya Transaksi', 'Expense',
             (SELECT account_id FROM accounts WHERE account_code='5.2'));
     END IF;
 END $$;
